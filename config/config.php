e4563e58a8809201e0e969bc3f1c1ed6ae927f39
𝔸𝕚𝕕𝕒𝕔𝕙𝕚_𝕄𝕒𝕩 <frithdedo@outlook.com>
2024-11-20 16:26:31 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@
 | It can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['botToken'] = "";
+$config['botToken'] = "7467077896:AAG66vWSlQxFd8rImuEmanMe9Oaq4B3XvgU";
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ $config['botToken'] = "";
 | ID can be obtained from https://telegram.dog/username_to_id_bot
 |
 */
-$config['adminID'] = "";
+$config['adminID'] = "5188998741";
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ $config['adminID'] = "";
 | ID can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['logsID'] =  "";
+$config['logsID'] =  "-1002499501124";
 
 /*
 |--------------------------------------------------------------------------
